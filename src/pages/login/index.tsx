4c344fb86819e628e59caa05d04fc52df97e1c7e
Lucas Gomes <lucaass1997rp@gmail.com>
2024-04-14 15:15:32 -0300

    mundança de cor no titulo do Login

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -33,7 +33,7 @@ export function Login() {
         <div className="flex w-full h-screen items-center justify-center flex-col">
             <Link to="/">
                 <h1 className="mt-11 text-white mb-7 font-bold text-5xl">Dev
-                    <span className="bg-gradient-to-r from-blue-600 to-blue-950 bg-clip-text text-transparent">Link</span>
+                    <span className="bg-gradient-to-r from-blue-600 to-blue-600 bg-clip-text text-transparent">Link</span>
                 </h1>
             </Link>
 
